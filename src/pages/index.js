f06b9de229c01d9a7c50591ab321184b7f25695d
machine01 <machine01@machine01s-MacBook-Pro.local>
2023-02-07 10:46:57 +0900

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styled from 'styled-components'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+
+    <>
+      <Container>
+        hello. my world
+      </Container>
+
+    </>
+  )
+}
+
+
+
+const Container = styled.div`
+  display: flex;
+
+  flex-direction: column; // row
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  color:red;
+  background-color: #f5f5f5;
+  `
